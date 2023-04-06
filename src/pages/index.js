6bf49803302be5bf2e08b6c065934d7237000a2a
fadingNA <94852295+fadingNA@users.noreply.github.com>
2023-04-05 21:57:38 -0400

    add link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 /*********************************************************************************
- *  WEB422 – Assignment 5
+ *  WEB422 – Assignment 6
  *  I declare that this assignment is my own work in accordance with Seneca Academic Policy.
  *  No part of this assignment has been copied manually or electronically from any other source
  *  (including websites) or distributed to other students.
  *
  *  Name: Nonthachai Plodthong  Student ID: 152487211 Date: 16/March/2023
+ *  vercel : https://artwork-chi.vercel.app/login
  *
  ********************************************************************************/
 
